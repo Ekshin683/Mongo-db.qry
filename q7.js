ef3ee9109376307a64ef28386434e59aa1b32719
Ekshin683 <ekshindeep@gmail.com>
2026-02-02 12:42:54 +0530

    update

--- q7.js
+++ q7.js
@@ -7,3 +7,42 @@ db.employees.updateOne(
         $set:{salary: 3500} // $ -> for operator
     }
 )
+db.employees.updateOne(
+    {
+        email:"cathy@gmail.com"
+    },
+    {
+        $inc:{salary: 3500} //inc -> increase to existing value
+    }
+)
+db.employees.updateMany(
+    {
+        
+    },
+    {
+        $inc:{salary: 1000} //update salary for everyone
+    }
+)
+
+db.employees.updateMany(
+    {
+        
+    },
+    {
+        $set:{points:1} //update points to every one
+    }
+)
+db.employees.updateOne(
+    {email:"brian@gmail.com"},
+    {$set:{points:10}} //update points to specific user
+)
+//if document not found it will not update
+db.employees.updateOne(
+    {email:"abc@gmail.com"},
+    {$set:{points:10}},
+    {upsert:true} //if document not found it will insert new document
+)
+
+db.employees.deleteOne(
+    {email:"abc@gmail.com"} //delete specific document
+)
