e8071ea39dc62ee161ec474df02d8f95bf6a1e6e
Ekshin683 <ekshindeep@gmail.com>
2026-02-02 12:04:48 +0530

    update

--- q6.js
+++ q6.js
@@ -1,2 +1,5 @@
 // db.employees.find({ department: "IT" })
 db.employees.find({ department: "HR",salary:{$gt: 3000} })
+db.employees.find({$and:[{department:"HR"},{salary:{$gt:3000}}]}) //and=array and both condition
+db.employees.find({$or:[{department:"HR"},{salary:{$gt:3000}}]}) //or it should either one
+db.employees.find({$or:[{department:"HR"},{salary:{$gt:3000}}]},{name:1,email:1,department:1})
