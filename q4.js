f0414fe05a2fb9efd2e8ffe5f488a14d46ed65b7
Ekshin683 <ekshindeep@gmail.com>
2026-02-02 11:26:04 +0530

    update

--- q4.js
+++ q4.js
@@ -1,4 +1,6 @@
-db.employees.find(
-    { department: "IT",salary: 3000},
-    {_id:0, name: 1,salary:1}
-)
+// db.employees.find(
+//     { department: "IT",salary: 3000},
+//     {_id:0, name: 1,salary:1}
+// )
+
+db.employees.find().limit(3)
